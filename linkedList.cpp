fd8138d4a2abd934aa9434653cba50524d407da7
Omer Can Bozkurt <omerbozkurt912@gmail.com>
2023-07-04 12:28:40 +0300

    rewrite

--- linkedList.cpp
+++ linkedList.cpp
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 typedef struct n{
-    int x;
-    n *next;
+	int data;
+	n* next;
 }node;
 
 int main(){
-    node *root;
-    root=(node*)malloc(sizeof(node));
-    root -> x=20;
-    root -> next=(node*)malloc(sizeof(node));
-    root -> next -> x=3;
-    root -> next -> next= (node*)malloc(sizeof(node));
-    root -> next -> next -> x=5;
-
-    return 0;
+	node* root;
+	root=(node*)malloc(sizeof(node));
+	root->data=9;
+	root->next=(node*)malloc(sizeof(node));
+	root->next->data=5;
+	root->next->next=(node*)malloc(sizeof(node));
+	root->next->next->data=6;
+	printf("%d \n%d \n%d",root->data,root->next->data,root->next->next->data);
 }
